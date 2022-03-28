e365b5175353f11b4e41be70a1ac5de1f63e7539
Kajee <poomrapee.kho@gmail.com>
2022-03-29 00:05:20 +0700

    add some method to board

--- domainLayer/board.java
+++ domainLayer/board.java
@@ -1,4 +1,14 @@
 package DomainLayer;
+
 public class Board{
-  
+  private int locationValue;
+
+  public int getLocation(){
+    return this.locationValue;
+  }
+  public int moveToLocation(diceValue){
+    this.locationValue = locationValue + diceValue;
+    return this.locationValue;
+  }
+
 }
