b719644fe909dbd899399d2a3a230b8fe7252b7e
Kajee <poomrapee.kho@gmail.com>
2022-04-04 12:53:56 +0700

    add locationPrice() to board

--- domainLayer/board.java
+++ domainLayer/board.java
@@ -3,6 +3,7 @@ package DomainLayer;
 public class Board{
   private int boardValue;
   private int locationValue;
+  private int locationPrice = 500;
 
   public Board(){
 
@@ -12,5 +13,9 @@ public class Board{
     this.locationValue = locationValue + diceValue;
     return this.locationValue;
   }
+  public int getLocationPrice(){
+    return locationPrice;
+  }
+
 
 }
